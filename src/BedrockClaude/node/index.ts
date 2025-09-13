8607b651dcbb2336d8a8c067940619ce263085d3
Gavin Payne <gavinapayne@gmail.com>
2025-09-13 17:07:49 +0400

    v1.0.29

--- src/BedrockClaude/node/index.ts
+++ src/BedrockClaude/node/index.ts
@@ -10,7 +10,7 @@ export function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
   
   return {
-    packageVersion: "1.0.25",
+    packageVersion: "1.0.28",
     type: "BedrockClaude",
     isService: false,
     name: "Bedrock Claude",
